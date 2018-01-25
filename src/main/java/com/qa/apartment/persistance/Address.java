3938d8fae15be4a8df491c2ef77cf91535b9f6b1
chapmanoo <chapmanoo@hotmail.co.uk>
2018-01-25 10:11:03 +0000

    address validation

--- src/main/java/com/qa/apartment/persistance/Address.java
+++ src/main/java/com/qa/apartment/persistance/Address.java
@@ -2,52 +2,37 @@ package com.qa.apartment.persistance;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Embeddable
 public class Address {
 
-	@Column(name = "apartment_number")
-	private String apartmentNum;
-
-	@Column(length = 15)
-	private String building;
-
 	@Column(length = 20)
+	@NotEmpty(message = "cannot be empty")
+	@Pattern(regexp = "^[A-zÀ-ÿ0-9 ]*$")
 	private String street;
 
 	@Column(length = 20)
+	@NotEmpty(message = "cannot be empty")
+	@Pattern(regexp = "^[A-zÀ-ÿ0-9 ]*$", message = "City cann")
 	private String city;
 
 	@Column(length = 9)
+	@NotEmpty(message = "cannot be empty")
+	@Pattern(regexp = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))[0-9][A-Za-z]{2})$", message = "Must be a valid UK postcode")
 	private String postCode;
 
 	public Address() {
 
 	}
 
-	public Address(String apartmentNum, String building, String street, String city, String postCode) {
+	public Address(String street, String city, String postCode) {
 		super();
-		this.apartmentNum = apartmentNum;
-		this.building = building;
 		this.street = street;
 		this.city = city;
 		this.postCode = postCode;
-	}
-
-	public String getApartmentNum() {
-		return apartmentNum;
-	}
-
-	public void setApartmentNum(String apartmentNum) {
-		this.apartmentNum = apartmentNum;
-	}
-
-	public String getBuilding() {
-		return building;
-	}
-
-	public void setBuilding(String building) {
-		this.building = building;
 	}
 
 	public String getStreet() {
