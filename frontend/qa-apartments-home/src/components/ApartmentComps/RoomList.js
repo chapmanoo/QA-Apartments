367bd6c340b12264fd1ce1785ede45f8107f2d78
Alasdair Stuart <AlasdairStuart@live.co.uk>
2018-01-26 15:55:49 +0000

    m

--- frontend/qa-apartments-home/src/components/ApartmentComps/RoomList.js
+++ frontend/qa-apartments-home/src/components/ApartmentComps/RoomList.js
@@ -6,17 +6,33 @@ class RoomList extends React.Component {
     this.state = {
       schedule:[]
     }
+
+    this.getSchedule = this.getSchedule.bind(this);
   }
-   
-  viewRoomDetails=(id)=>{
-         let fetchData = {
-          method: 'GET',
-        };
-        let url = `${baseUrl}schedule/json`;
-        fetch(url,fetchData)
-        .then(function(data){
-            if (data) return data.json()
-        })
+  
+
+  getSchedule = (id) =>{
+    let fetchData = {
+      method: 'GET'
+    };
+    let url = `${baseUrl}schedule/json`;
+    fetch(url,fetchData)
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(data){
+
+      let scheduleList = [];
+
+      for(let x in data){
+        scheduleList.push(data[x]);
+      }
+
+      this.setState({schedule: []})
+      this.setState({schedule: scheduleList})
+    });
+
+
   }
 
   render() {
@@ -24,7 +40,12 @@ class RoomList extends React.Component {
     return (
       <div>
          <tr>Room {room.roomId}</tr>
-         <tr><button onclick={()=>this.viewRoomDetails(room.roomId)}>view details</button></tr>
+         <button onClick={this.getSchedule(room.roomId)}> Get Room Schedule </button>
+         {
+              this.state.schedule.map((roomInfo) => (
+                <p key = {roomInfo.id}>Person:{roomInfo.personID.firstName + " " + roomInfo.personID.lastName} From:{roomInfo.from_date} To:{roomInfo.to_date}</p>
+              ))
+          }
       </div>
     );
   }
